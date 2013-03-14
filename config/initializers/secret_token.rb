0b0611a32a16cc314cd2e06a847f02a93d8f12f5
eliranmoyal <eliran.moyal1@gmail.com>
2013-03-14 23:01:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OshDesigns::Application.config.secret_token = 'ebb60eb1309f2807dd510e5ae347491dfd4e68dae9d3b69a2dd88c5adce5e421cb06a4aa5f535ddb408782e2d9efbc2b26805e2563bc2b1cb0053479f0a1ae8a'
