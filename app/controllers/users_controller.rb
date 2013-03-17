902b09ff8f86f2bf93ae49b3f5634af3eab6a0f9
eliranmoyal <eliran.moyal1@gmail.com>
2013-03-17 21:51:10 +0200

    show users show correct user , if no user redirect to root_path with alert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-before_filter :authenticate_user! 
-  def show
-  end
+	before_filter :authenticate_user! 
+	def show
+		begin
+			@user = User.find(params[:id])
+		rescue ActiveRecord::RecordNotFound  
+			redirect_to root_path , :alert => "No Such User"
+			return
+		end
+	end
 end
