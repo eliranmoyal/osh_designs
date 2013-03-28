ac1b414a76ab4dfa8dbd6d8193642faa3fb18d7b
eliranmoyal <eliran.moyal1@gmail.com>
2013-03-28 17:08:36 +0200

    controller for admin in order to mass-assign order user_id

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,19 +1,50 @@
 ActiveAdmin.register Order do
-  filter :state  , :as => :select , :collection => ["unwatched","in progress"]
+	# add filters
+	filter :state  , :as => :select , :collection => ["unwatched","in progress"]
 
-  scope :all, :default => true
-    scope :unwatched
-  scope :in_progress
+# add scopes
+scope :all, :default => true
+scope :unwatched
+scope :in_progress
+
+ # member_action :lock, :method => :put do
+ #      user = User.find(params[:id])
+ #      user.lock!
+ #      redirect_to {:action => :show}, :notice => "Locked!"
+ #    end
 
 
+#override controller
+controller do
+      # This code is evaluated within the controller class
+
+      def create
+      	@order = Order.new
+      	@order.accessible = :all 
+      	@order.attributes = params[:order]
+      	if @order.save
+      		redirect_to admin_orders_path 
+      	else
+      		redirect_to root_url
+      	end
+      end
+  end
 
   form do |f|
-      f.inputs "Details" do
-        f.input :state , :collection =>  ["unwatched","in progress"]
-        f.input :expected_end ,:label => "Publish Post At"
-        f.input :percentage
-      end
-      f.buttons
-    end
+  	f.inputs "Details" do
+  		if f.object.new_record?
+  			f.input :user
+  			f.input :title
+  			f.input :order_type 
+  			f.input :description 
+
+  		end
+  		f.input :state , :collection =>  ["unwatched","in progress"]
+  		f.input :expected_end ,:label => "When Will I Finish It" , :as => :date_select
+  		f.input :percentage
+  		f.input :price
+  	end
+  	f.buttons
+  end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,20 @@
 class Order < ActiveRecord::Base
+
 	attr_accessible :description, :expected_end, :percentage, :price, :state, :order_type , :title
 	belongs_to :user
 	validates :user_id, presence: true
+	attr_accessor :accessible
+	default_scope order: 'orders.created_at'
+	scope :in_progress  , where("orders.state ='in progress' ")
+	scope :unwatched  , where("orders.state ='unwatched' ")
 
-	 default_scope order: 'orders.created_at'
-	 scope :in_progress  , where("orders.state ='in progress' ")
-	 scope :unwatched  , where("orders.state ='unwatched' ")
+	private
+
+		def mass_assignment_authorizer(role = :default)
+			if accessible == :all
+				self.class.protected_attributes
+			else
+				super + (accessible || [])
+			end
+		end
 end
