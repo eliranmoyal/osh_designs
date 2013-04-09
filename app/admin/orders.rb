5a6a45461d3ab032a5b5a41e85c600585ac7654b
eliranmoyal <eliran.moyal1@gmail.com>
2013-04-09 21:17:09 +0300

    changed users

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register Order do
 	# add filters
-  
+
   def order_types
     ["Logo","Banner", "Web Site"]
   end
 
-	filter :order_type  , :as => :select , :collection => order_types
+  filter :order_type  , :as => :select , :collection => order_types
   filter :price 
   filter :percentage
   filter :expected_end
@@ -17,30 +17,30 @@ scope :unwatched
 scope :in_progress
 
 
-    index do
-      column :id 
-    	
-      column "Title" , :sortable => :title do |order|
-        link_to order.title , resource_path(order) 
-      end
+index do
+  column :id 
 
-    	column :order_type
-    	column :state
-      column "Price" ,:sortable => :price do |order|
-        number_to_currency order.price , :unit =>  "&#8362;"
-      end
-      column "Progress" , :sortable => :percentage do |order|
-        "#{order.percentage}%"
-      end
-      column "Estimated Finshed",:expected_end
-    	column "ordered by" , :sortable => :user_id do |order|
-    		link_to order.user.name, admin_user_path(order.user)
-    	end
-    	column "Actions" do |order|
-		render  'admins/order_actions' , :order => order
-    	end
-    	
-    end
+  column "Title" , :sortable => :title do |order|
+    link_to order.title , resource_path(order) 
+  end
+
+  column :order_type
+  column :state
+  column "Price" ,:sortable => :price do |order|
+    number_to_currency order.price , :unit =>  "&#8362;"
+  end
+  column "Progress" , :sortable => :percentage do |order|
+    "#{order.percentage}%"
+  end
+  column "Estimated Finshed",:expected_end
+  column "ordered by" , :sortable => :user_id do |order|
+    link_to order.user.name, admin_user_path(order.user)
+  end
+  column "Actions" do |order|
+    render  'admins/order_actions' , :order => order
+  end
+
+end
 
 #override controller
 controller do
@@ -61,24 +61,24 @@ controller do
       	@order.update_attributes(params[:order])
       	redirect_to admin_orders_path 
       end
-  end
+    end
 
-  form do |f|
-  	f.inputs "Details" do
-  		if f.object.new_record?
-  			f.input :user
-  			f.input :title
-  			f.input :order_type 
-  			f.input :description 
+    form do |f|
+     f.inputs "Details" do
+      if f.object.new_record?
+       f.input :user
+       f.input :title
+       f.input :order_type 
+       f.input :description 
 
-  		end
-  		f.input :state , :collection =>  ["unwatched","in progress"]
-  		f.input :expected_end ,:label => "When Will I Finish It" , :as => :date_select
-  		f.input :percentage
-  		f.input :price
-  	end
-  	f.buttons
-  end
+     end
+     f.input :state , :collection =>  ["unwatched","in progress","completed"]
+     f.input :expected_end ,:label => "When Will I Finish It" , :as => :date_select
+     f.input :percentage
+     f.input :price
+   end
+   f.buttons
+ end
 
 
 
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,63 @@
 ActiveAdmin.register User do
-  
+
+	config.batch_actions = true
+
+	filter :name
+	filter :email
+	filter :created_at
+
+	index do
+		selectable_column
+		column "Name" , :sortable => :name do |user|
+			link_to user.name , resource_path(user)
+		end
+		column :email
+		column :phone
+		
+		column "facebook link" do |user|
+			if !(user.uid.nil? || user.uid.blank?)
+				link_to user.name, "http://www.facebook.com/#{user.uid}"
+			else
+				"not via facebook"
+			end
+		end
+		column "Number Of Orders"   do |user|
+			user.orders.count
+		end
+		column "In Debt Of" do |user|
+			sum_not_paid = user.orders.select{|order| order.state != "paid" && order.price !=nil}
+							.sum{|order| order.price}
+							
+			number_to_currency sum_not_paid, :unit =>  "&#8362;"	
+		end
+		column "Joined At " , :created_at
+		default_actions
+	end
+
+	show :title => :name do
+		panel "Order History" do
+			table_for(user.orders) do
+				column :id 
+				column("Order", :sortable => :title) {|order| link_to "#{order.title}", admin_order_path(order) }
+				column("State")                   {|order| status_tag(order.state) }
+				# column("Date", :sortable => :checked_out_at){|order| pretty_format(order.checked_out_at) }
+				column("Total")                   {|order| number_to_currency order.price , :unit =>  "&#8362;"}
+			end
+		end
+		active_admin_comments
+	end
+
+	sidebar "Customer Details", :only => :show do
+		attributes_table_for user, :name, :email, :phone ,:created_at
+	end
+
+	sidebar "Order History", :only => :show do
+		attributes_table_for user do
+			last_order =   user.orders.sort_by{|x| x.created_at}.first
+			row("Last Order") {link_to  last_order.title , admin_order_path(last_order)}
+			row("Total Orders") { user.orders.count }
+			row("Total Value") { number_to_currency user.orders.sum(:price)  , :unit =>  "&#8362;"}
+		end
+	end
+
 end
