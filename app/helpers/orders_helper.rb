bac07a7ccd31bcb15be2b9eac03c1c1277f04773
eliranmoyal <eliran.moyal1@gmail.com>
2013-04-12 20:12:44 +0300

    removed helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,12 +1,12 @@
 module OrdersHelper
 
-	def orders_chart_series(orders, start_time)
-		orders_by_day = orders.where(:created_at => start_time.beginning_of_day..Time.zone.now.end_of_day).
-		group("date(created_at)").
-		select("date(created_at), sum(price) as price")
-		(start_time.to_date..Date.today).map do |date|
-			order = orders_by_day.detect { |order| order.created_at.to_date == date }
-			order && order.price.to_f || 0
-		end.inspect
-	end
+	# def orders_chart_series(orders, start_time)
+	# 	orders_by_day = orders.where(:created_at => start_time.beginning_of_day..Time.zone.now.end_of_day).
+	# 	group("date(created_at)").
+	# 	select("date(created_at), sum(price) as price")
+	# 	(start_time.to_date..Date.today).map do |date|
+	# 		order = orders_by_day.detect { |order| order.created_at.to_date == date }
+	# 		order && order.price.to_f || 0
+	# 	end.inspect
+	# end
 end
